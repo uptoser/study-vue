3159b962bb0f191eb68dcd6dda2dea971220fd2f
huanfengrtb <450052550@qq.com>
2018-01-26 19:42:59 +0800

    base

--- base.js
+++ base.js
@@ -43,3 +43,22 @@ var app6 = new Vue({
     message: 'Hello Vue!'
   }
 })
+// 定义名为 todo-item 的新组件
+Vue.component('todo-item', {
+  // todo-item 组件现在接受一个
+  // "prop"，类似于一个自定义特性。
+  // 这个 prop 名为 todo。
+  props: ['todo'],
+  template: '<li>{{todo.text}}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: '蔬菜' },
+      { id: 1, text: '奶酪' },
+      { id: 2, text: '随便其它什么人吃的东西' }
+    ]
+  }
+})
