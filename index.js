52cb4bf9caa43a6a75069585e11b3ac9a870516e
huanfengrtb <450052550@qq.com>
2018-01-26 16:34:55 +0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue! 123'
+  }
+});
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: '页面加载于 ' + new Date().toLocaleString()
   }
 })
